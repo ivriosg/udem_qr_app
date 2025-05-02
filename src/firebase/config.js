0925f2be558d29d4167dcac77d701b6063fb79dd
Ivan Rios <ivriosg@gmail.com>
2025-05-01 18:54:09 -0600

    Actualizar claves config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCchqNygFoLrkBUhF3CQ8MJPwRCmn7YI3Q",
-  authDomain: "registro-udem-checkin.firebaseapp.com",
-  projectId: "registro-udem-checkin",
-  storageBucket: "registro-udem-checkin.firebasestorage.app",
-  messagingSenderId: "184762574802",
-  appId: "1:184762574802:web:1a21f0b034faa927b08a0b",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
